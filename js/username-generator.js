47a4828fdaa6ab57232f44221465ab59a52c05ea
Nikita Brylev <nbrylevv@gmail.com>
2020-03-11 00:06:51 +0300

    Release v1.1.0

--- js/username-generator.js
+++ js/username-generator.js
@@ -1,23 +1,40 @@
-class App {
-  constructor() {
-    this.options = {
-      minSliceResult: 2,
-    };
-
-    this.watchButton();
-  }
-
-  static getRandom(min, max) {
+const Helpers = {
+  getRandom: function(min, max) {
     return min + Math.round((max - min) * Math.random());
-  }
-
-  static shuffleArray(array) {
+  },
+  shuffleArray: function (array) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
     }
 
     return array;
+  }
+};
+
+const DOMIds = {
+  form: 'generator-form',
+  input1: 'input1',
+  input2: 'input2',
+  input3: 'input3',
+  formSubmitButton: 'generator-submit',
+  result: 'generator-result',
+  resultHint: 'generator-hint',
+  resultGreetings: 'generator-greetings',
+  settingsShuffle: 'shuffle-checkbox',
+};
+
+let BUTTON_PRESSED = false;
+
+class UsernameGenerator {
+  constructor(options = {
+    minSliceResult: 2,
+  }) {
+    this.options = options;
+
+    this.loadValues();
+    this.watchSubmitButton();
+    this.watchFormReset();
   }
 
   get formData() {
@@ -34,37 +51,108 @@ class App {
   }
 
   get form() {
-    return document.getElementById('generator-form');
+    return document.getElementById(DOMIds.form);
+  }
+
+  get formInputs() {
+    return {
+      [DOMIds.input1]: document.getElementById(DOMIds.input1),
+      [DOMIds.input2]: document.getElementById(DOMIds.input2),
+      [DOMIds.input3]: document.getElementById(DOMIds.input3),
+    }
+  }
+
+  get formInputsValues() {
+    const { formInputs } = this;
+
+    return Object.keys(formInputs).reduce((acc, inputId) => {
+      acc[inputId] = formInputs[inputId].value;
+
+      return acc;
+    }, {});
   }
 
   get formSettings() {
     return {
-      shuffle: document.getElementById('shuffle-checkbox').checked,
+      shuffle: document.getElementById(DOMIds.settingsShuffle).checked,
     }
   }
 
-  get formButton() {
-    return document.getElementById('generator-button');
+  get formSubmitButton() {
+    return document.getElementById(DOMIds.formSubmitButton);
   }
 
   get resultWrapper() {
-    return document.getElementById('generator-result');
+    return document.getElementById(DOMIds.result);
   }
 
-  watchButton() {
-    this.formButton.addEventListener('click', () => {
+  watchSubmitButton() {
+    this.formSubmitButton.addEventListener('click', () => {
+      if (!this.formData.length) {
+        return;
+      }
+
       this.saveValues();
       this.generate();
+
+      if (!BUTTON_PRESSED) {
+        BUTTON_PRESSED = true;
+        document.getElementById(DOMIds.resultHint).style.display = 'none';
+        document.getElementById(DOMIds.resultGreetings).style.display = 'block';
+      }
     });
   }
 
+  watchFormReset() {
+    this.form.addEventListener('reset', () => {
+      window.location.search = '';
+      this.focusFirstInput();
+    });
+  }
+
+  focusFirstInput() {
+    this.formInputs[DOMIds.input1].focus();
+  }
+
+  focusGenerateButton() {
+    this.formSubmitButton.focus();
+  }
+
+  loadValues() {
+    const { search: originSearch } = window.location;
+    if (!originSearch.length) {
+      this.focusFirstInput();
+      return;
+    }
+
+    const searchData = originSearch.slice(1, originSearch.length).split('&');
+    const { formInputs } = this;
+
+    searchData.forEach(_ => {
+      const [ key, value ] = _.split('=');
+      formInputs[key].value = value;
+    });
+    this.focusGenerateButton();
+  }
+
   saveValues() {
-    // WIP
+    const { formInputsValues } = this;
+    const searchData = Object.keys(this.formInputsValues).reduce((acc, inputId) => {
+      const inputValue = formInputsValues[inputId];
+
+      if (inputValue) {
+        acc.push([inputId, inputValue]);
+      }
+      return acc;
+    }, []);
+    const stringSearchData = searchData.map(_ => _.join('=')).join('&');
+
+    window.history.replaceState(null, window.location.title, `${window.location.pathname}?${stringSearchData}`);
   }
 
   getPartOfValue(value) {
-    const start = App.getRandom(0, value.length - this.options.minSliceResult);
-    const sliceLength = App.getRandom(1, value.length);
+    const start = Helpers.getRandom(0, value.length - this.options.minSliceResult);
+    const sliceLength = Helpers.getRandom(1, value.length);
 
     return value.slice(start, start + sliceLength);
   }
@@ -74,15 +162,12 @@ class App {
       this.getPartOfValue(value).toLowerCase(),
     );
 
-    console.log(this.formSettings);
     if (this.formSettings.shuffle) {
-      values = App.shuffleArray(values);
+      values = Helpers.shuffleArray(values);
     }
 
     this.resultWrapper.innerHTML = values.join('');
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  new App();
-});
+window.UsernameGenerator = UsernameGenerator;
